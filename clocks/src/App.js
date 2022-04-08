dfa07854002f001b81bcf2b8b7e292c0b3aad0a8
oyako-li <oyakoli3@gmail.com>
2022-04-08 21:18:52 +0900

    update clocks app

--- clocks/src/App.js
+++ clocks/src/App.js
@@ -1,30 +1,50 @@
 import './App.css';
+import React, {useState} from 'react';
 
-const times = {
-  position: "absolute",
-  inset: "20px",
-  color: "#fff",
-  text_alange: "center",
-  transform: "rotate(calc(30deg*ver(--i)))",
+const TimeSign = () => {
+  const items = [];
+  for(let i=1; i<13; i++) items.push(<span style={{"--i": i}}><b>{i}</b></span>);
+  return (
+    <>
+      {items}
+    </>
+  )
+};
+
+const TimeHandrer = () => {
+  const [hh, setHh] = useState(0);
+  const [mm, setMm] = useState(0);
+  const [ss, setSs] = useState(0);
+  
+  // useEffect(()=>{
+  //   hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+  //   mn.style.transform = `rotateZ(${mm}deg)`;
+  //   sc.style.transform = `rotateZ(${ss}deg)`;
+  // })
+
+  setInterval(()=>{
+    let day = new Date();
+    setHh(day.getHours()*30);
+    setMm(day.getMinutes()*6);
+    setSs(day.getSeconds()*6);
+  }, 1000);
+
+  return (
+    <>
+      <div className="circle" id="hr" style={{"--clr": "#04fc43", transform: `rotateZ(${hh+(mm/12)}deg)`}}><i></i></div>;
+      <div className="circle circle2" id="mn" style={{"--clr": "#fee800", transform: `rotateZ(${mm}deg)`}}><i></i></div>;
+      <div className="circle circle3" id="sc" style={{"--clr": "#ff2972", transform: `rotateZ(${ss}deg)`}}><i></i></div>;
+    </>
+  )
 }
 
-const App = () => (
-  <div className="container">
-    <div className="clock" style={times}>
-      <span style={{"--i": 1}}><b>1</b></span>
-      <span style={{"--i": 2}}><b>2</b></span>
-      <span style={{"--i": 3}}><b>3</b></span>
-      <span style={{"--i": 4}}><b>4</b></span>
-      <span style={{"--i": 5}}><b>5</b></span>  
-      <span style={{"--i": 6}}><b>6</b></span>  
-      <span style={{"--i": 7}}><b>7</b></span>  
-      <span style={{"--i": 8}}><b>8</b></span>  
-      <span style={{"--i": 9}}><b>9</b></span>  
-      <span style={{"--i": 10}}><b>10</b></span>  
-      <span style={{"--i": 11}}><b>11</b></span>  
-      <span style={{"--i": 12}}><b>12</b></span>  
+const App = () => {
+  return (
+    <div className="clock">
+      <TimeSign />
+      <TimeHandrer />
     </div>
-  </div>
-)
+  )
+};
 
 export default App;
